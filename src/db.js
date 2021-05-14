14f84f7356af2582257aab0b5c60948080a01910
Zarif Ishmum <kzarifishmum@gmail.com>
2021-05-14 16:09:44 -0500

    added Schema for lists of tasks

--- src/db.js
+++ src/db.js
@@ -1,13 +1,25 @@
 const mongoose = require('mongoose');
 const URLSlugs = require('mongoose-url-slugs');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 // add your schemas
 const UserSchema = new mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
     username: {type: String, required: true},
     email: {type: String, required: true},
-    password: {type: String, unique: true, required: true},
 });
+
+const TaskSchema = new mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    listName: String,
+    currentTasks: [String],
+    completedTasks: [String],
+})
+
+UserSchema.plugin(passportLocalMongoose);
 
 // register your model
 mongoose.model('User', UserSchema);
-mongoose.connect('mongodb://localhost/noteApp');
+mongoose.model('TaskList', TaskSchema);
+mongoose.connect('mongodb://localhost/noteApp',  { useNewUrlParser: true });
