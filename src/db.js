7ffccee802a6b7b14ad92600cdacd37e96b79084
Zarif Ishmum <kzarifishmum@gmail.com>
2021-05-15 11:40:39 -0500

    updated schemas to properly show the relationship between different collections

--- src/db.js
+++ src/db.js
@@ -7,19 +7,28 @@ const UserSchema = new mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     username: {type: String, required: true},
     email: {type: String, required: true},
+    taskLists: [{type: mongoose.Schema.Types.ObjectId, ref: 'TaskList'}]
 });
 
-const TaskSchema = new mongoose.Schema({
+const TaskListSchema = new mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
     listName: String,
-    currentTasks: [String],
-    completedTasks: [String],
+    currentTasks: [{type: mongoose.Schema.Types.ObjectId, ref: 'Task'}],
+    completedTasks: [{type: mongoose.Schema.Types.ObjectId, ref: 'Task'}],
+})
+
+const TaskSchema =  new mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    taskList: {type: mongoose.Schema.Types.ObjectId, ref: "TaskList"},
+    content: String,
+    date: Date
 })
 
 UserSchema.plugin(passportLocalMongoose);
 
 // register your model
 mongoose.model('User', UserSchema);
-mongoose.model('TaskList', TaskSchema);
+mongoose.model('TaskList', TaskListSchema);
+mongoose.model('Task', TaskSchema);
 mongoose.connect('mongodb://localhost/noteApp',  { useNewUrlParser: true });
